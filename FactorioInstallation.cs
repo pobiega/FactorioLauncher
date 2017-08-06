dbbf1dbb0a3e2abec96e9d35a4fb2f2f8db2fb8f
pobiega <robin.aakerman@gmail.com>
2017-08-06 12:15:49 +0200

    Initial commit

---
+++ FactorioInstallation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Factorio.Launcher
+{
+    public class FactorioInstallation
+    {
+        public static string BinarySubpath = "bin\\x64\\factorio.exe";
+        public static string ModDirFileSubpath = "mod-dirs.json";
+        public readonly string FactorioPath;
+
+        public List<string> ModDirectoryList { get; }
+
+
+        public FactorioInstallation(string path)
+        {
+            if(!IsFactorioInstallation(path))
+                throw  new ArgumentException("Path must be a valid factorio installation.");
+
+            FactorioPath = path;
+            ModDirectoryList = GetOrGenerateModDirectoryList();
+        }
+
+        private List<string> GetOrGenerateModDirectoryList()
+        {
+            var moddirsFile = Path.Combine(FactorioPath, ModDirFileSubpath);
+            if (File.Exists(moddirsFile))
+            {
+                var fileContent = File.ReadAllText(moddirsFile);
+                return JsonConvert.DeserializeObject<List<string>>(fileContent);
+            }
+
+            var dirs = Directory.EnumerateDirectories(FactorioPath);
+
+            return (from dir in dirs let modListFile = Path.Combine(FactorioPath, dir, "mod-list.json") where File.Exists(modListFile) select dir).ToList();
+        }
+
+        public static bool IsFactorioInstallation(string path)
+        {
+            if (!Directory.Exists(path))
+                return false;
+
+            var bindir = Path.Combine(path, BinarySubpath);
+
+            if (!File.Exists(bindir))
+                return false;
+
+            return true;
+        }
+
+        public void Launch(string modDir)
+        {
+            var exePath = Path.Combine(FactorioPath, BinarySubpath);
+            var p = new Process();
+            p.StartInfo.WorkingDirectory = FactorioPath;
+            p.StartInfo.FileName = BinarySubpath;
+            p.StartInfo.Arguments = $"--mod-directory {modDir}";
+            p.Start();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Factorio.Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public FactorioInstallation FactorioInstance;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FactorioInstance = new FactorioInstallation(@"E:\games\factorio");
+            FactorioPathLabel.Content = FactorioInstance.FactorioPath;
+            foreach (var dir in FactorioInstance.ModDirectoryList)
+            {
+                var dinfo = new DirectoryInfo(dir);
+
+                ModDirListBox.Items.Add(dinfo.Name);
+            }
+        }
+
+        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchSelected();
+        }
+
+        private void ModDirListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LaunchButton.IsEnabled = true;
+        }
+
+        private void ModDirListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (LaunchButton.IsEnabled)
+            {
+                LaunchSelected();
+            }
+        }
+
+        private void LaunchSelected()
+        {
+            var dinfo = (string)ModDirListBox.SelectedItems[0];
+            FactorioInstance.Launch(dinfo);
+        }
+
+        private void AddNewButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var diag = new InputDialog("Create new mod-directory:","mods-folder");
+
+            if (diag.ShowDialog() == true)
+            {
+                FactorioInstance.Launch(diag.Answer);
+            }
+        }
+    }
+}
